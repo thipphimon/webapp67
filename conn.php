e1e7b6cdddeb9598e5c91ce0990cb10d17590e36
thipphimon <thipphimonsawangrat@gmail.com>
2024-09-26 07:35:46 +0700

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,8 @@
+<?php
+$conn = new mysqli('localhost','root','','student');
+$conn->query("SET NAMES utf8");
+if($conn->connect_error){
+   die("Connection Fail God damn it ". $conn->$conn_error);
+}
+
+?>
---
+++ insertbio.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>this page show inserting</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<form method="post" action="insertbiosuccess.php">
+        <p>
+ 
+            <label>รหัสนักศึกษา</label>
+            <input type="text" name="sid" id="sid">
+ 
+        </p>
+        <p>
+ 
+            <label>ชื่อ</label>
+            <input type="text" name="sname" id="sname">
+ 
+        </p>
+ 
+        <p>
+ 
+            <label>นามสกุล</label>
+ 
+            <input type="text" name="slastname" id="slastname">
+ 
+        </p>
+ 
+        <p>
+ 
+            <label>ที่อยู่</label>
+ 
+            <input type="text" name="address" id="address">
+ 
+        </p>
+ 
+        <p>
+ 
+            <label>เบอร์โทร</label>
+ 
+            <input type="text" name="telephone" id="telephone">
+ 
+        </p>
+        <input class="save" id="bun"type="submit" value="บันทึก">
+        <a href='mainmenu.php'> <button class="home"> Home </button></a>
+    </form>
+ 
+</body>
+</html>
