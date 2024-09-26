04e344212f036ff54568ae8fc2462ab9fe62d9ce
thipphimon <thipphimonsawangrat@gmail.com>
2024-09-26 16:07:06 +0700

    Delete movie directory

--- movie/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = new mysqli('localhost','root','','movie');
-$conn->query("SET NAMES utf8");
-if($conn->connect_error){
-    die("Connection Fail God damn it ". $conn->$conn_error);
-}
-
-?>
